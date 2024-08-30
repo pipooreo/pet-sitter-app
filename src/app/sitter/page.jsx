570843a8553d47e48fb9d776185b7b84617b9a27
Kanokphit <kanokphit55@gmail.com>
2024-08-30 15:53:30 +0700

    feat:create file provider.jsx

---
+++ src/app/sitter/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import React from "react";
+
+function Sitterpage() {
+  const { data: session } = useSession();
+  console.log(session);
+  const handleSignOut = async () => {
+    await signOut({ callbackUrl: "/loginsitter" });
+  };
+  return (
+    <div>
+      <button onClick={handleSignOut}>Log Out</button>{" "}
+    </div>
+  );
+}
+
+export default Sitterpage;
