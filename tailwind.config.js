c0a55e55ce7d00d84ec5ab175d6ea20c76594418
Dear <natthaphong.ea@gmail.com>
2024-09-02 11:03:53 +0700

    fix: rename page.js to page.jsx and update trailling comma on tailwind config page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        satoshi: ["Satoshi", "sans-serif"]
-      }
+        satoshi: ["Satoshi", "sans-serif"],
+      },
     },
     screens: {
       xs: "375px",
@@ -17,65 +17,65 @@ module.exports = {
       md: "768px",
       lg: "1024px",
       xl: "1280px",
-      custom: "1023px"
+      custom: "1023px",
     },
     fontSize: {
       head1: [
         "56px",
         {
           lineHeight: "64px",
-          fontWeight: "700"
-        }
+          fontWeight: "700",
+        },
       ],
       head2: [
         "36px",
         {
           lineHeight: "44px",
-          fontWeight: "700"
-        }
+          fontWeight: "700",
+        },
       ],
       head3: [
         "24px",
         {
           lineHeight: "32px",
-          fontWeight: "700"
-        }
+          fontWeight: "700",
+        },
       ],
       head4: [
         "20px",
         {
           lineHeight: "28px",
-          fontWeight: "700"
-        }
+          fontWeight: "700",
+        },
       ],
       body1: [
         "18px",
         {
           lineHeight: "26px",
-          fontWeight: "500"
-        }
+          fontWeight: "500",
+        },
       ],
       body2: [
         "16px",
         {
           lineHeight: "28px",
-          fontWeight: "500"
-        }
+          fontWeight: "500",
+        },
       ],
       body3: [
         "14px",
         {
           lineHeight: "24px",
-          fontWeight: "500"
-        }
+          fontWeight: "500",
+        },
       ],
       display: [
         "88px",
         {
           lineHeight: "96px",
-          fontWeight: "900"
-        }
-      ]
+          fontWeight: "900",
+        },
+      ],
     },
     colors: {
       gray: {
@@ -84,7 +84,7 @@ module.exports = {
         300: "#AEB1C3",
         400: "#7B7E8F",
         500: "#5B5D6F",
-        600: "#3A3B46"
+        600: "#3A3B46",
       },
       orange: {
         100: "#FFF1EC",
@@ -92,28 +92,28 @@ module.exports = {
         300: "#FFB899",
         400: "#FF986F",
         500: "#FF7037",
-        600: "#E44A0C"
+        600: "#E44A0C",
       },
       green: {
         100: "#E7FDF4",
-        500: "#1CCD83"
+        500: "#1CCD83",
       },
       yellow: {
         100: "#FFF5EC",
-        500: "#FFCA62"
+        500: "#FFCA62",
       },
       blue: {
         100: "#ECFBFF",
-        500: "#76D0FC"
+        500: "#76D0FC",
       },
       pink: {
         100: "#FFF0F1",
-        500: "#FA8AC0"
+        500: "#FA8AC0",
       },
       red: "#EA1010",
       white: "#FFFFFF",
-      black: "#000000"
-    }
+      black: "#000000",
+    },
   },
-  plugins: [require("daisyui")]
+  plugins: [require("daisyui")],
 };
