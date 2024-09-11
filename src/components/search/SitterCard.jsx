3e525b25131ee8ef27b2bc9791d08f282c5b5c42
PiPo <pipo.khomgrid@gmail.com>
2024-09-11 09:54:45 +0700

    feat: fixed error seach and added noresult component

---
+++ src/components/search/SitterCard.jsx
@@ -0,0 +1,104 @@
+"use client";
+
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { SlLocationPin } from "react-icons/sl";
+import { PiPawPrintDuotone } from "react-icons/pi";
+
+function SitterCard({ data }) {
+  //   console.log(sitter.id);
+  const [sitter, setSitter] = useState(data);
+
+  return (
+    <div
+      className="flex flex-col gap-4 items-center rounded-2xl bg-white w-full"
+      key={sitter.id}
+    >
+      <div className="rounded-2xl p-4 flex flex-col gap-4 xl:flex-row lg:gap-10 lg:w-full">
+        <img
+          src=""
+          className="w-[311px] h-[100px] rounded-lg lg:w-[245px] lg:h-[184px]"
+          // alt="sitter"
+        />
+        <div className="flex flex-col gap-2 lg:gap-6 lg:w-full">
+          <div className="flex gap-4 justify-between w-full">
+            <div className="flex gap-4 ">
+              <img
+                src=""
+                className="w-[36px] h-[36px] lg:w-[64px] lg:h-[64px] rounded-full"
+                //   alt="profile"
+              />
+              <div className="flex flex-col ">
+                <h3 className="text-black text-[18px] leading-6 font-bold lg:text-head3  ">
+                  {sitter.trade_name}
+                </h3>
+                <p className="text-body3 lg:text-body1 text-black">
+                  By {sitter.name}
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-[2px] p-[6px] rounded-lg">
+              {Array.from({ length: 5 }).map((_, index) => {
+                return (
+                  <FaStar
+                    className="text-green-500 w-[12px] h-[12px] lg:w-[20px] lg:h-[20px]"
+                    key={index}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div className="flex justify-between text-gray-400 items-center">
+            <div className="flex gap-[6px] text-gray-400 items-center">
+              <SlLocationPin className="w-[16px] h-[16px] lg:w-[24px] lg:h-[24px]" />
+              <p className="text-body3 lg:text-body2">Senanikom, Bangkok</p>
+            </div>
+            <div className="flex gap-[6px] text-gray-400 items-center mr-2">
+              <PiPawPrintDuotone className="w-[16px] h-[16px] lg:w-[24px] lg:h-[24px]" />
+              <p className="text-body3 lg:text-body2">
+                {sitter.experience} Years
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            {sitter.pet_type.map((item, index) => {
+              if (item) {
+                return item === "Dog" ? (
+                  <div
+                    className="rounded-[99px] p-[4px_16px] border border-green-500 bg-green-100 text-green-500"
+                    key={index}
+                  >
+                    {item}
+                  </div>
+                ) : item === "Cat" ? (
+                  <div
+                    className="rounded-[99px] p-[4px_16px] border border-pink-500 bg-pink-100 text-pink-500"
+                    key={index}
+                  >
+                    {item}
+                  </div>
+                ) : item === "Bird" ? (
+                  <div
+                    className="rounded-[99px] p-[4px_16px] border border-blue-500 bg-blue-100 text-blue-500"
+                    key={index}
+                  >
+                    {item}
+                  </div>
+                ) : (
+                  <div
+                    className="rounded-[99px] p-[4px_16px] border border-orange-500 bg-orange-100 text-orange-500"
+                    key={index}
+                  >
+                    {item}
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SitterCard;
