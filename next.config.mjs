347529d858fa476a98df98be0ecddae43b474172
Beam <jkpbeam@gmail.com>
2024-09-24 17:49:52 +0700

    fix: del vercel problem next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [process.env.NEXT_PUBLIC_SUPABASE_DOMAIN], // Add your Supabase URL here
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
