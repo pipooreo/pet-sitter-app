d7eee284d34633449713ac75b1a378effbbe862b
Dear <natthaphong.ea@gmail.com>
2024-09-24 13:49:52 +0700

    feat: add pet sitter profile and update pet sitter page

--- src/app/pet-sitter/page.jsx
+++ src/app/pet-sitter/page.jsx
@@ -10,26 +10,21 @@ import Header from "@/components/pet-sitter-profile/Header";
 import BasicInformationForm from "@/components/pet-sitter-profile/BasicInformationForm";
 import PetSitterForm from "@/components/pet-sitter-profile/PetSitterForm";
 import AddressForm from "@/components/pet-sitter-profile/AddressForm";
+import { PetSitterProfileProvider } from "@/context/PetSitterProfileContext";
 
 function Sitterpage() {
   const { data: session, status } = useSession();
-  const router = useRouter(); // เรียกใช้ useRouter
+  const router = useRouter();
 
-  const handleSignOut = async () => {
-    try {
-      await signOut({ redirect: false }); // ป้องกันการรีไดเรกต์ทันที
-      toast.success("Logged out successfully!"); // แสดง toast เมื่อออกจากระบบสำเร็จ
-      router.replace("/login/pet-sitter"); // นำทางไปยังหน้า login
-    } catch (error) {
-      toast.error("An error occurred while logging out.");
-    }
-  };
-
-  const handleHi = async () => {
-    try {
-      toast.info("HEHE XD");
-    } catch (error) {}
-  };
+  // const handleSignOut = async () => {
+  //   try {
+  //     await signOut({ redirect: false }); // ป้องกันการรีไดเรกต์ทันที
+  //     toast.success("Logged out successfully!"); // แสดง toast เมื่อออกจากระบบสำเร็จ
+  //     router.replace("/login/pet-sitter"); // นำทางไปยังหน้า login
+  //   } catch (error) {
+  //     toast.error("An error occurred while logging out.");
+  //   }
+  // };
 
   if (status === "loading") {
     return (
@@ -39,26 +34,31 @@ function Sitterpage() {
     );
   }
 
-  {
-    /* <Navbar session={session} /> */
+  // หากผู้ใช้ไม่ได้เข้าสู่ระบบ จะนำทางไปยังหน้าเข้าสู่ระบบ
+  if (status === "unauthenticated") {
+    router.replace("/login/pet-sitter");
+    return null; // ไม่ต้อง render อะไรในขณะนี้
   }
+
   {
     /* <button onClick={handleSignOut}>Log Out</button>
-  <button onClick={handleHi}> meow</button> */
+     */
   }
   return (
-    <div className="flex">
-      <aside className="bg-gray-200 w-1/6 "></aside>
-      <main className="w-5/6 ">
-        <NavbarSitter />
-        <section className="bg-gray-100 px-10 pt-5 pb-20 flex flex-col gap-6">
-          <Header />
-          <BasicInformationForm />
-          <PetSitterForm />
-          <AddressForm />
-        </section>
-      </main>
-    </div>
+    <PetSitterProfileProvider>
+      <div className="flex">
+        <aside className="bg-gray-200 w-1/6 "></aside>
+        <main className="w-5/6 ">
+          <NavbarSitter />
+          <section className="bg-gray-100 px-10 pt-5 pb-20 flex flex-col gap-6">
+            <Header />
+            <BasicInformationForm />
+            <PetSitterForm />
+            <AddressForm />
+          </section>
+        </main>
+      </div>
+    </PetSitterProfileProvider>
   );
 }
 
